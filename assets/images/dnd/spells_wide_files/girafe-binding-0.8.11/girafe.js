2b4c5e7a312003b410b80e44f365848b96975847
jessjep <150984462+jessjep@users.noreply.github.com>
2024-12-27 19:18:27 +1100

    Delete assets directory

--- assets/images/dnd/spells_wide_files/girafe-binding-0.8.11/girafe.js
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:5aa7ebaeec2f64615f179822be2ceee4ad4ed91f9a85a3457084616e33e4d92e
-size 113
--- assets/js/sticky-header.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener("scroll", function () {
-  if (window.scrollY > 55) {
-    document.getElementById("header").classList.add("sticky");
-  } else {
-    document.getElementById("header").classList.remove("sticky");
-  }
-});
