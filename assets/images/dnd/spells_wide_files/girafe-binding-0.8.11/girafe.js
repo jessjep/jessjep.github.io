7c2e58bf7ad912ca8d61143a1587abd9f75a3e3e
jessjep <jessicamoore.tn@gmail.com>
2024-12-27 19:12:20 +1100

    spells post

--- assets/images/dnd/spells_wide_files/girafe-binding-0.8.11/girafe.js
+++ assets/images/dnd/spells_wide_files/girafe-binding-0.8.11/girafe.js
@@ -1,5 +1,3 @@
-HTMLWidgets.widget({
-  name: 'girafe',
-  type: 'output',
-  factory: ggiraphjs.factory(HTMLWidgets.shinyMode)
-});
+version https://git-lfs.github.com/spec/v1
+oid sha256:5aa7ebaeec2f64615f179822be2ceee4ad4ed91f9a85a3457084616e33e4d92e
+size 113
